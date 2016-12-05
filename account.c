8704e890b9c7b6537294c447f968ddf505984e9a
EunsueBaek <acttoac7@naver.com>
2016-12-05 19:35:35 +0900

    account.ceeeeeeeee

---
+++ account.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "account.h"
+
+void AccUpdate(){
+	int jobnum;
+
+	printf("\n \t Account Update \n");
+	printf("\t\t 1. ID Change \n");
+	printf("\t\t 2.Passwd Change \n");
+	printf("\t 중 수행할 작업의 번호를 입력하세요 \n");
+
+	scanf(" %d ",&jobnum);
+}
