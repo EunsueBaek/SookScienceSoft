572ea612892391df417a5ff64d44ec63d10456e2
EunsueBaek <acttoac7@naver.com>
2016-12-07 16:14:04 +0900

    Print is changed(Korean -> English)

--- account.c
+++ account.c
@@ -7,11 +7,11 @@ void AccUpdate(){
 	printf("\n \t Account Update \n");
 	printf("\t\t 1. ID Change \n");
 	printf("\t\t 2.Passwd Change \n");
-	printf("\t 중 수행할 작업의 번호를 입력하세요 \n");
+	printf("\t Select Jobnumber! \n");
 
 	scanf(" %d ",&jobnum);
 }
 
 void Errmsg(void){
-	printf("바른값을 입력하세요.");
+	printf("Please insert right number!");
 }
